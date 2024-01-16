da2f8c2cda0f68b9252629a64cb1e647a7eb5a0a
tiantian.li@philips.com <tiantian.li@philips.com>
2024-01-16 09:43:48 +0000

    push

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-    "fmt"
-    "os"
+	"fmt"
+	"os"
 )
 
 func main() {
-    fmt.Fprintf(os.Stdout, "Hello world; public repo; 20240116 18:37")
+	fmt.Fprintf(os.Stdout, "Hello world; public repo; 20240116 18:43")
 }
